41da8057b23ead38d375336feeec7efcf8034dd7
lastnitescurry <Brian.Dinneen@gmail.com>
2016-07-27 16:20:12 +0100

    Demo to Dave G.

--- src/main/java/ie/farrengold/javaee7/HelloWorld.java
+++ src/main/java/ie/farrengold/javaee7/HelloWorld.java
@@ -30,7 +30,7 @@ public class HelloWorld {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getHtml() {
-		return "<html lang=\"en\"><body><h1>Hello, World!!</h1><h2>Gareth in the house</h2></body></html>";
+		return "<html lang=\"en\"><body><h1>Hello, World!!</h1><h2>David in the house</h2></body></html>";
 	}
 
 	@GET
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,6 +2,5 @@
 <body>
 <h2>Hello World! JSP</h2>
 <h2>What is not to like about tomcat parallel deployment feature</h2>
-<p>it is me</p>
 </body>
 </html>
