90853d812d2acbe6f36e34020a6aad812060d946
brian.dinneen <brian.dinneen@hmrc.gsi.gov.uk>
2016-11-10 21:07:07 +0000

    Refresh and update. Builds with: clean install site

--- src/main/java/ie/farrengold/javaee7/HelloWorld.java
+++ src/main/java/ie/farrengold/javaee7/HelloWorld.java
@@ -31,7 +31,7 @@ public class HelloWorld {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getHtml() {
-		return "<html lang=\"en\"><body><h1>Hello, World!! How are you</h1></body></html>";
+		return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
 	}
 
 	/**
